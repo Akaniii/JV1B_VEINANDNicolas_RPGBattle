4b7da3d6e44230055d56e25bcdd6081ae643b8e9
Akaniii <95611827+Akaniii@users.noreply.github.com>
2022-12-10 17:34:24 +0100

    Add files via upload

---
+++ rpgbattle/projetrpgbattle.js
@@ -0,0 +1,15 @@
+projetrpgbattle
+
+hppersonnage1=100;
+hppersonnage2=100;
+hppersonnage3=100;
+
+expPersonnage1=0;
+expPersonnage2=0;
+expPersonnage3=0;
+
+hpmonstre1=50;
+hpmonstre2=50;
+hpmonstre3=50;
+
+attaqueperso=10;
